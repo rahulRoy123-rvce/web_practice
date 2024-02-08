dcd7e17c07cdc806c34d463214385813188de8f7
Rahul Roy <rahulroy.ec21@rvce.edu.in>
2024-02-08 18:28:21 +0530

    project

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Navbar from '../components/shared/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
